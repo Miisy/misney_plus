def1a23b7b6a5eea4e6162a46eb9c623b4868ca2
Thamires Sales <thamisales@hotmail.com>
2024-08-17 13:17:41 -0300

    adding tabs changes and js for change tabs.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,33 @@
 document.addEventListener(`DOMContentLoaded`, function(){
-    const buttons = document.querySelectorAll('[data-tab-button')
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    
+
+    for (let i = 0 ; i<buttons.length; i++) {
+        buttons[i].addEventListener(`click`, function(botao) {
+            const targetTab = botao.target.dataset.tabButton;
+            const buttonTarget = botao.target
+            const tab = document.querySelector(`[data-tab-id = ${targetTab}]`);
+            hideAllTabs();
+            hideUnderline();
+            tab.classList.add('shows__list--is-active');
+            botao.target.classList.add('shows__tabs__button--is-active')
+        })
+    }
 })
+
+function hideAllTabs() {
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
+
+    for( let i = 0; i < tabsContainer.length; i++){
+        tabsContainer[i].classList.remove('shows__list--is-active');
+    }
+}
+
+function hideUnderline(){
+    const buttons = document.querySelectorAll('[data-tab-button]');
+
+    for (let i = 0; i < buttons.length; i++){
+        buttons[i].classList.remove('shows__tabs__button--is-active');
+    }
+}
